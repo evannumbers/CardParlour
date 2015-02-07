0afafc944fec0e72453dfd94a9b693304858f380
Jesse Anderson <jessea@andrew.cmu.edu>
2015-02-07 00:27:43 -0500

    Additional server features

--- src/server/server.js
+++ src/server/server.js
@@ -2,30 +2,57 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+function CAH() {
+  this.players = [];
+  this.deck = [];
+  this.graveyard = [];
+
+  function Player(nme, sckt) {
+    this.name = nme;
+    this.socket = sckt;
+  }
+
+  this.sendState = function(player, state) {
+    player.socket.emit('update state', state);
+  };
+
+  this.addPlayer = function(name, socket) {
+    player = new Player(name, socket);
+    this.players[this.players.length] = player
+    this.sendState(player, 0);
+    console.log("Player added");
+  };
+}
+
+var cah = new CAH();
+
 app.get('/', function(req, res){
   res.sendFile('clients/player.html', {'root': '../'});
-  io.on('connection', function(socket){
-    console.log("player connected");
-    socket.on('join', function(name){
-      //Tell game that a player joined
-      console.log("player joined");
-    });
-    socket.on('play', function(id){
-      //Tell game that a card was played
-    });
-    socket.on('czarFlip', function(id){
-      //Tell game that the czar flipped a card
-    });
-    socket.on('disconnect', function(){
-      //Tell game that the player left
-    });
-  });
+
 });
 
 app.get('/display', function(req, res){
   res.sendFile('clients/display.html', {'root': '../'});
-  io.on('connection', function(socket){
-    //Tell the game a display joined
+});
+
+io.on('connection', function(socket){
+  console.log("client connected");
+  socket.on('join', function(name){
+    //Tell game that a player joined
+    cah.addPlayer(name, socket);
+  });
+  socket.on('play', function(id){
+    //Tell game that a card was played
+  });
+  socket.on('czar flip', function(id){
+    //Tell game that the czar flipped a card
+  });
+  socket.on('disconnect', function(){
+    //Tell game that the player/display left
+    console.log("client disconnected");
+  });
+  socket.on('register display', function(){
+    //Tell the game a display has joined
   });
 });
 
